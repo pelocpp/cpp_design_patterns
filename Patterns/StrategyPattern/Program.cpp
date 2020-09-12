39adbfc0ea189bcb1f2079197b801d0a82dcfec9
Peter Loos <peter.loos@gmx.de>
2020-09-12 18:21:55 +0200

    moved 'render' example to case studies project

--- Patterns/StrategyPattern/Program.cpp
+++ Patterns/StrategyPattern/Program.cpp
@@ -5,14 +5,10 @@
 // function prototypes
 void test_conceptual_example();
 void test_ecommerce_app();
-void test_render_app_dynamically();
-void test_render_app_statically();
 
 int main() {
     test_conceptual_example();
     test_ecommerce_app();
-    test_render_app_dynamically();
-    test_render_app_statically();
     return 0;
 }
 
