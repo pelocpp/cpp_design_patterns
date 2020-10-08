d40f5637314a58bd1c43f350472a6e635ddaadc4
Peter Loos <peter.loos@gmx.de>
2020-10-08 16:58:54 +0200

    code cleaning 'dynamic shapes'

--- Patterns/DecoratorPattern/DynamicShapes.cpp
+++ Patterns/DecoratorPattern/DynamicShapes.cpp
@@ -3,7 +3,7 @@
 #include <sstream>
 #include <memory>
 
-// component
+// corresponds to 'component'
 class Shape
 {
 public:
@@ -11,7 +11,7 @@ public:
     virtual std::string draw() const = 0;
 };
 
-// concrete ccomponent
+// corresponds to 'concrete component'
 class Circle : public Shape {
 private:
     float m_radius; 
@@ -29,7 +29,7 @@ public:
     }
 };
 
-// concrete component
+// corresponds to 'concrete component'
 class Square : public Shape
 {
 private:
@@ -48,7 +48,7 @@ public:
     }
 };
 
-// base decorator class
+// corresponds to 'base decorator class'
 class Decorator : public Shape {
 protected:
     std::shared_ptr<Shape> m_component;
@@ -57,13 +57,13 @@ public:
     Decorator(const std::shared_ptr<Shape>& component) 
       : m_component(component) {}
 
-    // decorator delegates all work to the wrapped componen
+    // base decorator class delegates all work to the wrapped component
     std::string draw() const override {
         return m_component->draw();
     }
 };
 
-// concrete decorator class 
+// corresponds to 'concrete decorator class'
 class ColoredShape : public Decorator
 {
 private:
@@ -76,12 +76,12 @@ public:
     std::string draw() const override
     {
         std::ostringstream oss;
-        oss << Decorator::draw() << " has the color " << m_color;
+        oss << Decorator::draw() << " has color " << m_color;
         return oss.str();
     }
 };
 
-// another concrete decorator class 
+// corresponds to another 'concrete decorator class' 
 class TransparentShape : public Decorator
 {
 private:
@@ -95,7 +95,7 @@ public:
     {
         std::ostringstream oss;
         oss << Decorator::draw() << " has "
-            << static_cast<uint8_t>(m_transparency) / 255.f * 100.f
+            << m_transparency / 255.f * 100.f
             << "% transparency";
         return oss.str();
     }
@@ -106,7 +106,7 @@ void test_dynamic_decoration_01() {
     std::shared_ptr<Shape> redCircle 
         = std::make_shared<ColoredShape>(circle, "red");
     std::cout << redCircle->draw() << std::endl;
-    // "A circle of radius 0.5 has the color red"
+    // "A circle of radius 0.5 has color red"
 }
 
 void test_dynamic_decoration_02() {
@@ -118,10 +118,10 @@ void test_dynamic_decoration_02() {
 }
 
 void test_dynamic_decoration_03() {
-    std::shared_ptr<Shape> circle = std::make_shared<Circle>(static_cast<float>(23));
+    std::shared_ptr<Shape> circle = std::make_shared<Circle>(static_cast<float>(15));
     std::shared_ptr<Shape> greenCircle = std::make_shared<ColoredShape>(circle, "green");
     std::shared_ptr<Shape> greenTransparentCircle 
-        = std::make_shared<TransparentShape>(greenCircle, static_cast<uint8_t>(64));
+        = std::make_shared<TransparentShape>(greenCircle, static_cast<uint8_t>(50));
     std::cout << greenTransparentCircle->draw() << std::endl;
-    // "A circle of radius 23 has the color has 25.098% transparency"
+    // "A circle of radius 15 has color green has 19.6078% transparency"
 }
