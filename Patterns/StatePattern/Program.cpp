d506ec27ae5c5f222da11cb5f6664255516c5ee2
Peter Loos <peter.loos@gmx.de>
2022-07-17 18:08:33 +0200

    minor changes

--- Patterns/StatePattern/Program.cpp
+++ Patterns/StatePattern/Program.cpp
@@ -11,10 +11,10 @@ void test_departmentstore_example();
 
 int main()
 {
-    //test_conceptual_example_01();
-    //test_conceptual_example_02();
-    //test_conceptual_example_03();
-   // test_jobapplication_example();
+    test_conceptual_example_01();
+    test_conceptual_example_02();
+    test_conceptual_example_03();
+    test_jobapplication_example();
     test_departmentstore_example();
 
     return 0;
